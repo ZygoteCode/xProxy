db73c7874b785b6ac46bef44f4b0d4ede631f2b3
ZygoteCode <zygotecode@proton.me>
2025-04-20 22:17:22 +0200

    First commit

---
+++ ProxyConnection.cs
@@ -0,0 +1,32 @@
+﻿public class ProxyConnection
+{
+    public string Hostname { get; set; }
+    public uint Port { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public ProxyConnection(string hostname, uint port, string username = null, string password = null)
+    {
+        Hostname = hostname;
+        Port = port;
+        Username = username;
+        Password = password;
+    }
+
+    public bool HasCredentials()
+    {
+        return Username != null && Password != null && Username != "" && Password != "";
+    }
+
+    public override string ToString()
+    {
+        string proxy = $"{Hostname}:{Port}";
+
+        if (HasCredentials())
+        {
+            proxy += $"{Username}:{Password}";
+        }
+
+        return proxy;
+    }
+}
